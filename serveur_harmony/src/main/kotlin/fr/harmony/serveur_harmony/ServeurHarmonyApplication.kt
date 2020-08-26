01743dfdbda003cbc8341381ed397c13ded57ad3
MakiiX <t.bagnolati@gmail.com>
2020-08-26 14:39:51 +0200

    ajout du serveur

---
+++ serveur_harmony/src/main/kotlin/fr/harmony/serveur_harmony/ServeurHarmonyApplication.kt
@@ -0,0 +1,11 @@
+package fr.harmony.serveur_harmony
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServeurHarmonyApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServeurHarmonyApplication>(*args)
+}
