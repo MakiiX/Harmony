52a6805e429b6e97e823a51999b8578c744d2ad9
Bahixo <bahixo@hotmail.fr>
2020-08-27 14:45:19 +0200

    ajout CURRENT USER et correction URL

--- Harmony_Client/src/main/kotlin/model/constant.kt
+++ Harmony_Client/src/main/kotlin/model/constant.kt
@@ -2,5 +2,7 @@ package model
 private const val URL_SERV="racine"
 const val URL_SERV_REGISTER="$URL_SERV/register"
 const val URL_SERV_LOGIN="$URL_SERV/login"
-const val URL_SERV_SEND_MSG="$URL_SERV/send_msg"
-const val URL_SERV_REQUEST_LIST_MSG="$URL_SERV/request_list_msg"
+const val URL_SERV_SEND_MSG="$URL_SERV/sendMsg"
+const val URL_SERV_REQUEST_LIST_MSG="$URL_SERV/requestListMsg"
+
+var CURRENT_USER:UserBean?=null
