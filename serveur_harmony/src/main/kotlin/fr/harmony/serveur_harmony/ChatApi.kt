2fbc48aa8ce092094e884e2d4a7d45c9e50ae616
MakiiX <t.bagnolati@gmail.com>
2020-08-28 11:50:19 +0200

    try catch & show request println

--- serveur_harmony/src/main/kotlin/fr/harmony/serveur_harmony/ChatApi.kt
+++ serveur_harmony/src/main/kotlin/fr/harmony/serveur_harmony/ChatApi.kt
@@ -7,34 +7,6 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 
-//TODO println chaque start requêtes
-//TODO Gérer tous les try catch
-
-/* register & login
-{
-  "id":null,
-  "login":"Toto",
-  "pwd":"motdepasse85",
-  "idSession":null
-}
-*/
-
-/*
-{
-  "id": null,
-  "text": "Mon superbe message",
-  "date": null,
-  "user": {
-    "id": null,
-    "login": null,
-    "pwd": null,
-    "idSession": 2951871338625046000
-  }
-}
-*/
-
-
-
 //Indique que c'est un webService
 @RestController
 class ChatApi {
@@ -42,15 +14,14 @@ class ChatApi {
     //Méthode test..
     @GetMapping("/test")
     fun test(): String {
-        println("Test")
+        println("/test")
         return "Hello World"
     }
 
     //Enregistre un nouvel utilisateur et retourne un ResponseApiBean en réponse
     @PostMapping("/register")
     fun register(@RequestBody user: UserBean): DataPackaged {
-
-        println("Tentative d'enregistrement...")
+        println("/register \nuser: $user")
         try {
             addUser(user)
             return DataPackaged(ResponseApiEnumBean.OK.rab)
@@ -66,28 +37,34 @@ class ChatApi {
     //Connecte l'utilisateur et lui reconnais un idSession
     @PostMapping("/login")
     fun login(@RequestBody user: UserBean): DataPackaged {
-        println("Tentative de connexion..")
+        println("/login \nuser: $user")
 
-        val check = checkPwd(user)
+        try {
 
-        //Vérifie si le nom user est bien déjà enregistrer
-        if (check == null) return DataPackaged(ResponseApiEnumBean.ERR_UNKNOW_USER.rab)
+            val check = checkPwd(user)
 
-        //Vérifie le mot de passe
-        else if (check == false) return DataPackaged(ResponseApiEnumBean.ERR_WRONG_PWD.rab)
+            //Vérifie si le nom user est bien déjà enregistrer
+            if (check == null) return DataPackaged(ResponseApiEnumBean.ERR_UNKNOW_USER.rab)
 
-        //Retour positif, modifie l' idSession de l'user de la listeUser, et retourne celui-ci au client (avec l' idSession)
-        else {
-            val userOnList = listUser.find { it.login == user.login && it.pwd == user.pwd }
-            userOnList!!.idSession = randomIdSession()
-            return DataPackaged(ResponseApiEnumBean.USER_OK.rab, userOnList)
+            //Vérifie le mot de passe
+            else if (check == false) return DataPackaged(ResponseApiEnumBean.ERR_WRONG_PWD.rab)
+
+            //Retour positif, modifie l' idSession de l'user de la listeUser, et retourne celui-ci au client (avec l' idSession)
+            else {
+                val userOnList = listUser.find { it.login == user.login && it.pwd == user.pwd }
+                userOnList!!.idSession = randomIdSession()
+                return DataPackaged(ResponseApiEnumBean.USER_OK.rab, userOnList)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return DataPackaged(ResponseApiEnumBean.ERR_UNKNOW_ERR.rab)
         }
+
     }
 
     //Réceptionne un message d'un utilisateur, lui donne une date et l'ajoute à la liste des messages (BDD)
     @PostMapping("/sendMsg")
     fun receiveMsg(@RequestBody msg: MsgBean): DataPackaged {
-
         println("/sendMsg \n msg = $msg")
 
         try {
@@ -115,6 +92,13 @@ class ChatApi {
     //Retourne un DataPackaged avec la liste des messages
     @GetMapping("/requestListMsg")
     fun sendListMsg(): DataPackaged {
-        return DataPackaged(ResponseApiEnumBean.MSG_LIST_OK.rab, listMsg = listMsgs)
+        println("/requestListMsg")
+        try {
+            return DataPackaged(ResponseApiEnumBean.MSG_LIST_OK.rab, listMsg = listMsgs)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return DataPackaged(ResponseApiEnumBean.ERR_UNKNOW_ERR.rab)
+        }
     }
 }
